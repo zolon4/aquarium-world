4f3031f4b0e9047c2a9ad5fcd58688e6ef897847
zolon4 <zolonsk8@gmail.com>
2020-04-11 17:34:34 -0500

    use soundcloud iframe

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,29 @@ const streams = [
 ];
 
 const Home = () => {
-  var url = streams[Math.floor(Math.random() * streams.length)];
+  let url = streams[Math.floor(Math.random() * streams.length)];
+  let audioUrl =
+    "https://whoopsadaisy.s3-us-west-2.amazonaws.com/aquarium/0003.mp3";
 
   return (
-    <div className="w-screen h-screen ">
-      <ReactPlayer url={url} playing muted width="100%" height="100%" />
-      <div className="hidden">
-        <ReactPlayer
-          url="https://soundcloud.com/zolon-wilkins/0003a"
-          playing
-          soundcloudConfig={{ preload: true }}
-        />
+    <div>
+      <Head>
+        <title>aquarium by zolon</title>
+        <meta property="og:title" content="aquarium" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="w-screen h-screen">
+        <ReactPlayer url={url} playing muted width="100%" height="100%" />
+        <div className="hidden">
+          <iframe
+            width="100%"
+            height="300"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1030469980&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
