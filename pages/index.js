3f19586c357e85bf72d9eb26f378e3ae878c5a63
zolon4 <zolonsk8@gmail.com>
2020-04-11 17:35:48 -0500

    hopefully this loops

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ const Home = () => {
             height="300"
             scrolling="no"
             frameborder="no"
-            allow="autoplay"
+            allow="autoplay loop"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1030469980&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
           ></iframe>
         </div>
