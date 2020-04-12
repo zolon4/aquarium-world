1064fb27a9a7d44399641207150e8b9d8177808a
zolon4 <zolonsk8@gmail.com>
2020-04-11 20:05:19 -0500

    add break all

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="loading-bg z-10 w-screen h-screen fixed flex items-center justify-center">
-      <h1 className="w-8/12 text-10xl text-white">Loading</h1>
+      <h1 className="w-8/12 text-10xl text-white break-all">Loading</h1>
     </div>
   );
 };
